1cf51ca398be0ff2f653bc0b32c5366dc9b0a70b
BenWhann <whann004@umn.edu>
2023-09-21 18:43:49 -0500

    squashed bug

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -5,38 +5,38 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App.jsx';
-import About from './pages/about.jsx';
-import Contact from './pages/contact.jsx';
-import Portfolio from './pages/portfolio.jsx';
-import Resume from './pages/resume.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
 
-// const router = createBrowserRouter([
-//     {
-//       path: '/',
-//       element: <App />,
-//       errorElement: <ErrorPage />,
-//       children: [
-//         {
-//           index: true,
-//           element: <About />,
-//         },
-//         {
-//           path: '/contact',
-//           element: <Contact />,
-//         },
-//         {
-//           path: '/portfolio',
-//           element: <Portfolio />,
-//         },
-//         {
-//             path: '/resume',
-//             element: <Resume />,
-//         },
-//       ],
-//     },
-//   ]);
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <About />,
+        },
+        {
+          path: '/contact',
+          element: <Contact />,
+        },
+        {
+          path: '/portfolio',
+          element: <Portfolio />,
+        },
+        {
+            path: '/resume',
+            element: <Resume />,
+        },
+      ],
+    },
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <RouterProvider router={router} />
-    <App />
+    <RouterProvider router={router} />
 )
