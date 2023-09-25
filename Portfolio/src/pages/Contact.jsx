f10b69ce1df96ac2587f3f3fcb31106f2a291f04
BenWhann <whann004@umn.edu>
2023-09-25 18:19:15 -0500

    began formatting portfolio section

--- Portfolio/src/pages/Contact.jsx
+++ Portfolio/src/pages/Contact.jsx
@@ -3,14 +3,14 @@ export default function Contact() {
         <>
         <body>
             <h1>Contact Me</h1>
-            <form>
-                <label for="name">First Name:</label>
-                <input type="text" id="name" name="name"></input>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email"></input>
-                <label for="message">Message:</label>
-                <input type="text" id="message" name="message"></input>
-                <button type="button">Submit</button>
+            <form className="mt-3 mb-3 container-md">
+                <label className="form-label" for="name">First Name:</label>
+                <input className="form-control" type="text" id="name" name="name"></input>
+                <label className="form-label" for="email">Email:</label>
+                <input className="form-control" type="email" id="email" name="email"></input>
+                <label className="form-label" for="message">Message:</label>
+                <input className="form-control" type="text" id="message" name="message"></input>
+                <button className="mt-3 btn btn-secondary" type="button">Submit</button>
             </form>
         </body>
         </>
--- Portfolio/src/pages/Portfolio.jsx
+++ Portfolio/src/pages/Portfolio.jsx
@@ -3,7 +3,56 @@ export default function Portfolio() {
         <>
         <body>
             <h1>Portfolio Page</h1>
-            <p>Add projects and links to them</p>
+            <div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="..."></img>
+                    <div class="card-body">
+                        <h5 class="card-title">Project 1</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GitHub Link</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="..."></img>
+                    <div class="card-body">
+                        <h5 class="card-title">Project 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GitHub Link</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="..."></img>
+                    <div class="card-body">
+                        <h5 class="card-title">Project 3</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GitHub Link</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="..."></img>
+                    <div class="card-body">
+                        <h5 class="card-title">Project 4</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GitHub Link</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="..."></img>
+                    <div class="card-body">
+                        <h5 class="card-title">Project 5</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GitHub Link</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="..."></img>
+                    <div class="card-body">
+                        <h5 class="card-title">Project 6</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GitHub Link</a>
+                    </div>
+                </div>
+            </div>
         </body>
         </>
     );
