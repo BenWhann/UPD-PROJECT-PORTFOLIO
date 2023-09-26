ae444a9c4ea6234772ad6ac6f95200737bb09b35
BenWhann <whann004@umn.edu>
2023-09-26 18:21:23 -0500

    goat

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
         <body>
             <h1 className="">About Page</h1>
             <p className="">I'll write my bio later. For now, enjoy this picture of me.</p>
-            <img className="yes" src="/public/assets/yes.jpg" alt="..."></img>
+            <img className="yes" src="https://th.bing.com/th?id=OIP.zdspTTKp3M4gTr20HtJH5gHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="..."></img>
         </body>
         </>
     );
